372336ae678a2078878698fe697c0393b816030a
Katie Harris <katherineharris@ou.edu>
2020-02-12 21:41:36 -0600

    k

--- QR Wifi 417 Elm/QR Wifi 417 Elm/MainWindow.xaml.cs
+++ QR Wifi 417 Elm/QR Wifi 417 Elm/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using QRCoder;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static QRCoder.PayloadGenerator;
 
 namespace QR_Wifi_417_Elm
 {
@@ -23,6 +27,51 @@ namespace QR_Wifi_417_Elm
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnNormanGen_Click(object sender, RoutedEventArgs e)
+        {
+            WiFi generator = new WiFi("katiejordan", "blackbanana1", WiFi.Authentication.WPA);
+            string payload = generator.ToString();
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeAsBitmap = qrCode.GetGraphic(20);
+            imgNorman.BeginInit();
+            imgNorman.Source = BitmapToImageSource(qrCodeAsBitmap);
+            imgNorman.EndInit();
+        }
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+
+        }
+
+        private void btnTulsaGen_Click(object sender, RoutedEventArgs e)
+        {
+            WiFi generator = new WiFi("brenternet_att", "katieanna", WiFi.Authentication.WPA);
+            string payload = generator.ToString();
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeAsBitmap = qrCode.GetGraphic(20);
+            imgTulsa.BeginInit();
+            imgTulsa.Source = BitmapToImageSource(qrCodeAsBitmap);
+            imgTulsa.EndInit();
+
         }
     }
 }
